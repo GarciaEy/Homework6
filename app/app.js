a4bed0c083d3dd0e8e382cae41a2df69e3dc16e5
Eyanla Garcia <eygarcia@iu.edu>
2024-11-03 19:08:30 -0500

    login page

--- app/app.js
+++ app/app.js
@@ -1,25 +1,27 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-//const hamburgerMenu = $("hamburgerMenu");
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const nav = document.querySelector(".nav");
 
-const nav = document.querySelector(".nav");
+  if (hamburgerMenu) {
+    hamburgerMenu.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
 
-hamburgerMenu.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
+  function initListeners() {
+    $(".nav a").on("click", function (e) {
+      let id = e.currentTarget.id;
+      console.log(id);
+      $("#app").html(id);
+      nav.classList.toggle("active");
+    });
 
-function initListeners() {
-  $(".nav a").on("click", function (e) {
-    let id = e.currentTarget.id;
-    console.log(id);
-    $("#app").html(id);
-    nav.classList.toggle("active");
+    $(".nav").on("click", function (e) {
+      nav.classList.toggle("active"); /*or remove*/
+    });
+  }
+
+  $(document).ready(function () {
+    initListeners();
   });
-
-  $(".nav").on("click", function (e) {
-    nav.classList.toggle("active"); /*or remove*/
-  });
-}
-
-$(document).ready(function () {
-  initListeners();
 });
